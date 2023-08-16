f3d2ae2bcb7bab3e863a3dad79900ec23b06a761
musesum <musesum@gmail.com>
2023-08-16 15:24:15 +0800

    xrOS initial

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,8 @@ let package = Package(
         .package(url: "https://github.com/musesum/MuPar.git", from: "0.23.0"),
         .package(url: "https://github.com/musesum/MuFlo.git", from: "0.23.0"),
         .package(url: "https://github.com/musesum/MuPeer.git", from: "0.23.0"),
-        .package(url: "https://github.com/AudioKit/AudioKit.git", from: "5.0.0"),
+        //.package(url: "https://github.com/warpling/AudioKit.git", branch: "visionOS"),
+        .package(path: "../AudioKit-visionOS"),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.0"))
     ],
 
@@ -25,7 +26,7 @@ let package = Package(
             .product(name: "MuPar", package: "MuPar"),
             .product(name: "MuFlo", package: "MuFlo"),
             .product(name: "MuPeer", package: "MuPeer"),
-            .product(name: "AudioKit", package: "AudioKit"),
+            .product(name: "AudioKit", package: "AudioKit-visionOS"),
             .product(name: "Collections", package: "swift-collections")
         ]),
         .testTarget(
